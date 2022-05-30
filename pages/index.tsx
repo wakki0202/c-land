77d54ec9788585c7c669e33513bb2851e69a55e4
wakki0202 <ytk0202@outlook.jp>
2022-05-30 13:12:24 +0900

    jsx => tsx changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      
+      aaaaa
     </div>
   )
 }
